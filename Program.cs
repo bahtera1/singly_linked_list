af1e20696b9d5a784b9da9332c315c0b2b08ed36
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2022-11-11 20:04:17 +0700

    add case 1-4

--- Program.cs
+++ Program.cs
@@ -126,7 +126,76 @@ namespace singly_linked_list
 
         static void Main(string[]args)
         {
-            
+            list obj = new list();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("\nMENU");
+                    Console.WriteLine("1. add a record to the list");
+                    Console.WriteLine("2. delete a record from the list");
+                    Console.WriteLine("3. view all the records in the list");
+                    Console.WriteLine("4. Search the records in the list");
+                    Console.WriteLine("5. EXIT");
+                    Console.Write("\n Enter your choice (1-5)");
+                    char ch = Convert.ToChar(Console.ReadLine());
+
+                    switch(ch)
+                    {
+                        case '1':
+                            {
+                                obj.addNote();
+                            }
+                            break;
+                        case '2':
+                            {
+                                if (obj.listEmpty())
+                                {
+                                    Console.WriteLine("\n List is Empty");
+                                    break;
+                                }
+                                Console.WriteLine("\n List is Empty");
+                                break ;
+                            }
+                            Console.Write("\nEnter the roll number of"+
+                                "the students whose record is to be deleted :");
+                            int nim=Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine();
+                            if(obj.delNode(nim)== false)
+                                Console.WriteLine("\n Record not found");
+                            else
+                                Console.WriteLine("Record with roll number"+ nim+"deleted");    
+                    }
+                    break;
+                 case '3':
+                    {
+                        obj.traverse();
+                    }
+                    break;
+                case '4':
+                    {
+                        if(obj.listEmpty()== true)
+                        {
+                            Console.WriteLine("\n list is empty");
+                            break;
+                        }
+                        Node previous, current;
+                        previous = current = null;
+                        Console.Write("\nEnter the roll number of the+" +
+                            "students whose records is to be searched");
+                        int num =Convert.ToInt32(Console.ReadLine());
+                        if (obj.Search(num,ref previous,ref current)== false)
+                            Console.WriteLine('\nrecord not found');
+                        else
+                        {
+                            Console.WriteLine("\n Record not found");
+                            Console.WriteLine('\n Roll number'+ current.rollNumber);
+                            Console.WriteLine('\n Name' + current.name);
+                        }
+
+                    }
+                }
+            }
         }
     }
 
